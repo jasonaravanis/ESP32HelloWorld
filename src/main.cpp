a47caf07244d327291b51fdf04e991c2b538b8b0
Jason Aravanis <j.aravanis@icloud.com>
2023-03-13 13:19:06 +1100

    feat: wifi connected, non-blocking

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,33 @@
 #include <Arduino.h>
-#include <WiFiMulti.h>
+#include <WiFi.h>
 
 #define WIFI_SSID "WIFI_NAME"
 #define WIFI_PASSWORD "WIFI_PASSWORD"
-
-WiFiMulti wifiMulti;
 
 void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
-  Serial.println("Hello from the setup");
-
-  wifiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
-  while (wifiMulti.run() != WL_CONNECTED)
-  {
-    delay(100);
-    Serial.println("...");
-  }
-
-  Serial.println("Connected");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.println("starting");
 }
 
+bool isConnected = false;
+
 void loop()
+
 {
-  digitalWrite(LED_BUILTIN, WiFi.status() == WL_CONNECTED);
+  if (WiFi.status() == WL_CONNECTED && !isConnected)
+  {
+    Serial.println("Connected");
+    digitalWrite(LED_BUILTIN, HIGH);
+    isConnected = true;
+  }
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    isConnected = false;
+    Serial.println(".");
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    delay(1000);
+  }
 }
